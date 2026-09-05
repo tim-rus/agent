2aad5d84190e35a04434f70f9013fce123cacf19
Tim-Rus <timmymulla@yandex.ru>
2026-09-05 12:16:10 +0500

    ref(core): extract user messge formatting

--- core/internal/chat/request.go
+++ core/internal/chat/request.go
@@ -13,7 +13,7 @@ import (
 
 func (chat *Chat) request(ctx context.Context, msg string) (*openai.ChatCompletion, error) {
 	union := msgsToUnions(chat.history)
-	u := append(union, messageToUnion(Message{Role: RoleUser, Message: fmt.Sprintf("<user_input>\n%s\n</user_input>", msg)}))
+	u := append(union, messageToUnion(formatUserMessage(msg)))
 
 	res, err := chat.client.Chat.Completions.New(
 		ctx,
@@ -29,6 +29,18 @@ func (chat *Chat) request(ctx context.Context, msg string) (*openai.ChatCompleti
 	}
 
 	return res, nil
+}
+
+func formatUserMessage(msg string) Message {
+	return Message{
+		Role: RoleUser,
+		Message: fmt.Sprintf(
+			`<user_input>
+				%s
+			</user_input>`,
+			msg,
+		),
+	}
 }
 
 func responseFormat(schema map[string]any) openai.ChatCompletionNewParamsResponseFormatUnion {
