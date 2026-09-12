5d00a1eb3cbbfa85dc22174b9722a9db14452cb4
Tim-Rus <timmymulla@yandex.ru>
2026-09-12 19:28:30 +0500

    feat(core.llm): request

--- core/internal/llm/llm.go
+++ core/internal/llm/llm.go
@@ -1,9 +1,12 @@
 package llm
 
 import (
+	"context"
 	"errors"
+	"fmt"
 
 	"github.com/openai/openai-go/v3"
+	"github.com/openai/openai-go/v3/option"
 )
 
 //
@@ -22,4 +25,23 @@ func New(client openai.Client) *LLM {
 	return &LLM{
 		client: client,
 	}
+}
+
+//
+
+func (llm *LLM) Request(ctx context.Context, params openai.ChatCompletionNewParams, thinking bool) (openai.ChatCompletionChoice, error) {
+	res, err := llm.client.Chat.Completions.New(
+		ctx,
+		params,
+		option.WithJSONSet("enable_thinking", thinking),
+	)
+	if err != nil {
+		return openai.ChatCompletionChoice{}, fmt.Errorf("failed to make openai request: %w", err)
+	}
+
+	if len(res.Choices) == 0 {
+		return openai.ChatCompletionChoice{}, ErrEmptyResult
+	}
+
+	return res.Choices[0], nil
 }
