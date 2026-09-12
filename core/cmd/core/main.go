c67875ea06c27e8a799d941ae72a0e62d60a1439
Tim-Rus <timmymulla@yandex.ru>
2026-09-12 20:57:03 +0500

    ref(core.llm)!: move params to llm struct

--- core/cmd/core/main.go
+++ core/cmd/core/main.go
@@ -74,7 +74,7 @@ func run(cfg Config) error {
 		option.WithBaseURL(os.Getenv("OPENAI_BASE_URL")),
 	)
 
-	llm := llm.New(oai)
+	llm := llm.New(oai, cfg.Models.Default)
 	dialog := dialog.New(llm)
 
 	_ = dialog
--- core/internal/llm/llm.go
+++ core/internal/llm/llm.go
@@ -19,20 +19,27 @@ var (
 
 type LLM struct {
 	client openai.Client
+	params openai.ChatCompletionNewParams
 }
 
-func New(client openai.Client) *LLM {
+func New(client openai.Client, model string) *LLM {
 	return &LLM{
 		client: client,
+		params: openai.ChatCompletionNewParams{
+			Model: model,
+		},
 	}
 }
 
 //
 
-func (llm *LLM) Request(ctx context.Context, params openai.ChatCompletionNewParams, thinking bool) (openai.ChatCompletionChoice, error) {
+func (llm *LLM) Request(ctx context.Context, msgs []Message, thinking bool) (openai.ChatCompletionChoice, error) {
+	p := llm.params
+	p.Messages = messagesToUnion(msgs)
+
 	res, err := llm.client.Chat.Completions.New(
 		ctx,
-		params,
+		p,
 		option.WithJSONSet("enable_thinking", thinking),
 	)
 	if err != nil {
@@ -44,4 +51,21 @@ func (llm *LLM) Request(ctx context.Context, params openai.ChatCompletionNewPara
 	}
 
 	return res.Choices[0], nil
+}
+
+//
+
+func messagesToUnion(msgs []Message) []openai.ChatCompletionMessageParamUnion {
+	union := make([]openai.ChatCompletionMessageParamUnion, len(msgs))
+	for i, m := range msgs {
+		switch m.Role {
+		case RoleSystem:
+			union[i] = openai.SystemMessage(m.Content)
+		case RoleAssistant:
+			union[i] = openai.AssistantMessage(m.Content)
+		default:
+			union[i] = openai.UserMessage(m.Content)
+		}
+	}
+	return union
 }
