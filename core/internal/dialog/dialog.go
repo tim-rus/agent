6b21100123df85242fa4404c61df66719a2aa007
Tim-Rus <timmymulla@yandex.ru>
2026-09-12 21:27:20 +0500

    feat(core.dialog): ask

--- core/internal/dialog/dialog.go
+++ core/internal/dialog/dialog.go
@@ -1,6 +1,12 @@
 package dialog
 
-import "core/internal/llm"
+import (
+	"context"
+	"core/internal/llm"
+	"fmt"
+)
+
+//
 
 type Dialog struct {
 	llm     *llm.LLM
@@ -19,4 +25,24 @@ func New(l *llm.LLM, systemPrompt string) *Dialog {
 		},
 		usage: llm.Usage{},
 	}
+}
+
+//
+
+func (d *Dialog) Ask(ctx context.Context, q string) (string, error) {
+	msg := llm.Message{Role: llm.RoleUser, Content: q}
+
+	// copy history in case of request error
+	msgs := make([]llm.Message, len(d.history)+1)
+	copy(msgs, d.history)
+	msgs[len(d.history)] = msg
+
+	res, err := d.llm.Request(ctx, msgs, false)
+	if err != nil {
+		return "", fmt.Errorf("failed to request llm: %w", err)
+	}
+
+	d.history = append(d.history, msg, llm.Message{Role: llm.RoleAssistant, Content: res.Content})
+
+	return res.Content, nil
 }
