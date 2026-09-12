511910a03ceade52938049de06996b4c440deaa3
Tim-Rus <timmymulla@yandex.ru>
2026-09-12 15:00:57 +0500

    feat(core): .env file loader

--- core/cmd/core/main.go
+++ core/cmd/core/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "log/slog"
+import (
+	"log/slog"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	if err := godotenv.Load("../config/.core.local.env"); err != nil {
+		slog.Error("failed to load env file", "err", err)
+		os.Exit(1)
+	}
+
 	if err := cliLoop(); err != nil {
 		slog.Error("cli loop failed", "err", err)
 	}
--- core/go.mod
+++ core/go.mod
@@ -1,3 +1,5 @@
 module core
 
 go 1.27.0
+
+require github.com/joho/godotenv v1.5.1
