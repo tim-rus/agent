de1c16967c4af3ca6a8cebcfb7f354f357ccbf82
Tim-Rus <timmymulla@yandex.ru>
2026-09-13 00:26:16 +0500

    sketc(tg): ping client

--- tg/main.py
+++ tg/main.py
@@ -2,17 +2,29 @@ import asyncio
 from os import getenv
 from pathlib import Path
 from dotenv import load_dotenv
+import yaml
+
+from pg.ping import PINGServiceStub
+from grpclib.client import Channel
 
 from tgmanager import TG
 import routing
 
 # env
 
-dotenv_file_env = getenv("ENV_FILE", "../config/.env.tg")
+dotenv_file_env = getenv("ENV_FILE", "./.env")
 dotenv_file_path = Path(dotenv_file_env).resolve()
 
 if not load_dotenv(dotenv_path=dotenv_file_path):
 	raise RuntimeError(f"Failed to load env file at: {dotenv_file_path}")
+
+# config
+
+config_file_env = getenv("CONFIG_FILE", "./.config.yaml")
+config_file_path = Path(config_file_env).resolve()
+
+with open(config_file_path, "r") as file:
+    config = yaml.safe_load(file)
 
 # params
 
@@ -24,16 +36,21 @@ proxyString = getenv("PROXY_STRING")
 if not proxyString:
 	raise RuntimeError(f"PROXY_STRING required")
 
-# services
-
-bot = TG(token=tgToken, proxy_url=proxyString)
-routing.setupRoutes(bot.dp)
 
 # main
 
 async def main():
+	channel = Channel(host=config['rpc']['host'], port=config['rpc']['port'])
+	stub = PINGServiceStub(channel)
+
+	bot = TG(token=tgToken, proxy_url=proxyString)
+	routing.setupRoutes(bot.dp, stub)
+
 	print("Starting bot")
 	await bot.connect()
+
+	# cleanup
+	channel.close()
 
 # run
 
--- tg/routing.py
+++ tg/routing.py
@@ -1,12 +1,15 @@
 from aiogram import Dispatcher
 from aiogram.types import Message
+from pg.ping import PINGServiceStub
 
-def setupRoutes(dp: Dispatcher):
+def setupRoutes(dp: Dispatcher, rpc: PINGServiceStub):
 
 	@dp.message()
 	async def msg_handler(message: Message) -> None:
 		try:
 			await message.send_copy(chat_id=message.chat.id)
+			res = await rpc.ping(msg=message.text)
+			print(f"msg: {res.msg}")
 
 		except TypeError:
 			await message.answer("Unsupported message")
