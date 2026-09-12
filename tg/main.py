cd3c46603246b3ae5e1c0149e2cbad868689500b
Tim-Rus <timmymulla@yandex.ru>
2026-09-12 23:12:46 +0500

    feat(tg): load env file

--- tg/main.py
+++ tg/main.py
@@ -1,1 +1,15 @@
-print("hi")
+from os import getenv
+from pathlib import Path
+from dotenv import load_dotenv
+
+# env
+
+dotenv_file_env = getenv("ENV_FILE", "../config/.env.tg")
+dotenv_file_path = Path(dotenv_file_env).resolve()
+
+if not load_dotenv(dotenv_path=dotenv_file_path):
+	raise RuntimeError(f"Failed to load env file at: {dotenv_file_path}")
+
+# 
+
+print(f"test: {getenv("TEST")}")
