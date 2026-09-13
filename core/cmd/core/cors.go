4db62e9ae39bb7272483c8132184c3e89cca0b69
Tim-Rus <timmymulla@yandex.ru>
2026-09-13 14:36:42 +0500

    feat(core.rpc): cors

---
+++ core/cmd/core/cors.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+// (!) ai generated
+func corsAllowAll(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Connect-Protocol-Version")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
