16078713a64ba50505212dd1d6e837d98db8fe45
Tim-Rus <timmymulla@yandex.ru>
2026-09-13 14:07:58 +0500

    chore(ui): load env file to vite

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,20 +3,41 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv'
+import fs from "fs"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-	tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(import.meta.dirname, "./src"),
-    },
-  },
-  server: {
-	port: 3000,
-  }
+export default defineConfig(({mode})=>{
+	const envDir = process.env.CONFIG_PATH || "."
+	const targetEnvFile = path.resolve(import.meta.dirname, envDir, process.env.ENV_FILE || '.env')
+
+	let parsedEnv:any = {}
+	if (fs.existsSync(targetEnvFile)) {
+		const envConfig = fs.readFileSync(targetEnvFile)
+		parsedEnv = dotenv.parse(envConfig)
+	}
+
+	const envInjection = Object.keys(parsedEnv).reduce((acc:any, key) => {
+		acc[`import.meta.env.${key}`] = JSON.stringify(parsedEnv[key])
+		return acc
+	}, {})
+
+	return {
+		plugins: [
+			react(),
+			babel({ presets: [reactCompilerPreset()] }),
+			tailwindcss(),
+		],
+		resolve: {
+			alias: {
+				"@": path.resolve(import.meta.dirname, "./src"),
+			},
+		},
+		envDir,
+		mode: process.env.MODE || mode,
+		server: {
+			port: parsedEnv.PORT ? Number(parsedEnv.PORT) : 3000,
+		},
+		define: envInjection,
+	}
 })
